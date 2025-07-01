083b3e028aca1d26b32297e74f56610885b76cfb
Eduard Parvu <eduard.parvu@rinftech.com>
2025-07-01 15:58:37 +0300

    - reading from a sql file

--- src/main/java/com/example/sk/administration/database/domain/TableInfo.java
+++ src/main/java/com/example/sk/administration/database/domain/TableInfo.java
@@ -3,9 +3,10 @@ package com.example.sk.administration.database.domain;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
-public record TableInfo(@NullMarked String tableName,
-                        @NullMarked String columnName,
-                        @NullMarked String dataType,
-                        @Nullable   String targetTable,
-                        @Nullable   String targetColumn) {
+@NullMarked
+public record TableInfo(String tableName,
+                        String columnName,
+                        String dataType,
+                        @Nullable String targetTable,
+                        @Nullable String targetColumn) {
 }
